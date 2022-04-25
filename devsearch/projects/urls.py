2d5645605a0424cff6ea7d34a52929e57d5b6e11
Dima Pasichnyi <dpasichnyi@23andme.com>
2022-04-25 14:26:55 -0700

    add project list using djenga

--- devsearch/projects/urls.py
+++ devsearch/projects/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 
 
 urlpatterns = [
-    path('projects/', views.projects, name="projects"),
+    path('', views.projects, name="projects"),
     path('project/<str:pk>/', views.project, name="project"),
 ]
--- devsearch/projects/views.py
+++ devsearch/projects/views.py
@@ -1,9 +1,39 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
+
+projectlist = [
+    {
+        'id': '1',
+        'title': 'Ecommerce Website',
+        'description': 'Fully functional ecommerce website.'
+    },
+    {
+        'id': '2',
+        'title': 'Portfolio Website',
+        'description': 'This was a project where I built out my portfolio.'
+    },
+    {
+        'id': '3',
+        'title': 'Social Network',
+        'description': 'Awesome open-source project I am still working on.'
+    },
+]
+
+
 def projects(request):
-    return render(request, 'projects/projects.html')
+    page = "projects"
+    number = 11
+    context = {
+        'page': page,
+        'number': number,
+        'projects': projectlist
+    }
+    return render(request, 'projects/projects.html', context)
 
 def project(request, pk):
-    return render(request, 'projects/single-project.html')
+    project_obj = None
+    for i in projectlist:
+        if i['id'] == pk:
+            project_obj = i
+    return render(request, 'projects/single-project.html', {'project': project_obj})
