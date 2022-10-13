6b8f4a3de7d3838ee3ddf5367648990808da23e6
simplydv <simplydv@protonmail.com>
2022-10-13 11:49:22 -0700

    clean up

--- devsearch/users/models.py
+++ devsearch/users/models.py
@@ -1,7 +1,7 @@
-from unicodedata import name
 import uuid
-from django.db import models
+
 from django.contrib.auth.models import User
+from django.db import models
 
 
 class Profile(models.Model):
